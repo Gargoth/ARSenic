6d9f0cc282c44d02c4c2d41172ea518e911450c9
Christian Jay F. Rosales <gargoth13@gmail.com>
2024-02-18 23:22:37 +0800

    feat: Added gamemanager functions for authentication

--- Assets/Scripts/GameManagerScript.cs
+++ Assets/Scripts/GameManagerScript.cs
@@ -1,10 +1,27 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
+using System.Threading.Tasks;
+using Unity.Services.Authentication;
+using Unity.Services.Core;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 
 public class GameManagerScript : Singleton
 {
+    async void Awake()
+    {
+        try
+        {
+            // Initialize the Unity Services SDK
+            await UnityServices.InitializeAsync();
+        }
+        catch (Exception e)
+        {
+            Debug.LogException(e);
+        }
+    }
+    
     public static void DoLog(string log)
     {
         Debug.Log(log);
@@ -13,5 +30,47 @@ public class GameManagerScript : Singleton
     public static void MoveToScene(string sceneName) 
     {
         SceneManager.LoadScene(sceneName);
+    }
+    
+    async Task SignUpWithUsernamePassword(string username, string password)
+    {
+        try
+        {
+            await AuthenticationService.Instance.SignUpWithUsernamePasswordAsync(username, password);
+            Debug.Log("SignUp is successful.");
+        }
+        catch (AuthenticationException ex)
+        {
+            // Compare error code to AuthenticationErrorCodes
+            // Notify the player with the proper error message
+            Debug.LogException(ex);
+        }
+        catch (RequestFailedException ex)
+        {
+            // Compare error code to CommonErrorCodes
+            // Notify the player with the proper error message
+            Debug.LogException(ex);
+        }
+    }
+
+    async Task SignInWithUsernamePasswordAsync(string username, string password)
+    {
+        try
+        {
+            await AuthenticationService.Instance.SignInWithUsernamePasswordAsync(username, password);
+            Debug.Log("SignIn is successful.");
+        }
+        catch (AuthenticationException ex)
+        {
+            // Compare error code to AuthenticationErrorCodes
+            // Notify the player with the proper error message
+            Debug.LogException(ex);
+        }
+        catch (RequestFailedException ex)
+        {
+            // Compare error code to CommonErrorCodes
+            // Notify the player with the proper error message
+            Debug.LogException(ex);
+        }
     }
 }
