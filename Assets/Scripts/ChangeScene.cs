416ee140cafb25b1c998f976768cd68f1bfb15be
ldpunsalan <ldpunsalan@up.edu.ph>
2024-02-18 17:37:07 +0800

    refactor: change button logic
    change scene takes in string instead of int

--- Assets/Scripts/ChangeScene.cs
+++ Assets/Scripts/ChangeScene.cs
@@ -5,8 +5,8 @@ using UnityEngine.SceneManagement;
 
 public class ChangeScene : MonoBehaviour
 {
-    public void MoveToScene(int sceneID) 
+    public void MoveToScene(string sceneName) 
     {
-        SceneManager.LoadScene(sceneID);
+        SceneManager.LoadScene(sceneName);
     }
 }
