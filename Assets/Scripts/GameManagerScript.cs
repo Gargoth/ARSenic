e96deec458c7a42357d91bb4731a7dca12acb449
Christian Jay F. Rosales <gargoth13@gmail.com>
2024-02-23 12:42:23 +0800

    feat: Implemented LoginManagerScript with working debug functions

--- Assets/Scripts/GameManagerScript.cs
+++ Assets/Scripts/GameManagerScript.cs
@@ -7,7 +7,7 @@ using Unity.Services.Core;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 
-public class GameManagerScript : Singleton
+public class GameManagerScript : Singleton<GameManagerScript>
 {
     async void Awake()
     {
@@ -30,47 +30,5 @@ public class GameManagerScript : Singleton
     public static void MoveToScene(string sceneName) 
     {
         SceneManager.LoadScene(sceneName);
-    }
-    
-    async Task SignUpWithUsernamePassword(string username, string password)
-    {
-        try
-        {
-            await AuthenticationService.Instance.SignUpWithUsernamePasswordAsync(username, password);
-            Debug.Log("SignUp is successful.");
-        }
-        catch (AuthenticationException ex)
-        {
-            // Compare error code to AuthenticationErrorCodes
-            // Notify the player with the proper error message
-            Debug.LogException(ex);
-        }
-        catch (RequestFailedException ex)
-        {
-            // Compare error code to CommonErrorCodes
-            // Notify the player with the proper error message
-            Debug.LogException(ex);
-        }
-    }
-
-    async Task SignInWithUsernamePasswordAsync(string username, string password)
-    {
-        try
-        {
-            await AuthenticationService.Instance.SignInWithUsernamePasswordAsync(username, password);
-            Debug.Log("SignIn is successful.");
-        }
-        catch (AuthenticationException ex)
-        {
-            // Compare error code to AuthenticationErrorCodes
-            // Notify the player with the proper error message
-            Debug.LogException(ex);
-        }
-        catch (RequestFailedException ex)
-        {
-            // Compare error code to CommonErrorCodes
-            // Notify the player with the proper error message
-            Debug.LogException(ex);
-        }
     }
 }
