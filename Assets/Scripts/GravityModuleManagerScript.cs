c0bab5bd3a9e9ac70a76e9de2754ceb446b158a5
Christian Jay F. Rosales <gargoth13@gmail.com>
2024-03-03 00:05:19 +0800

    feat: Implemented hittest wrapper

--- Assets/Scripts/GravityModuleManagerScript.cs
+++ Assets/Scripts/GravityModuleManagerScript.cs
@@ -1,11 +1,40 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using Vuforia;
 
 public class GravityModuleManagerScript : Singleton<GravityModuleManagerScript>
 {
     [SerializeField] private GameObject objectContainer;
-    
+    [SerializeField] private ContentPositioningBehaviour _contentPositioningBehaviour;
+
+    void Update()
+    {
+        if (Input.touchCount > 0)
+        {
+            for (int i = 0; i < Input.touchCount; i++)
+            {
+                Touch touch = Input.GetTouch(i);
+                if (touch.phase != TouchPhase.Began) continue;
+                Ray ray = Camera.main.ScreenPointToRay(touch.position);
+                RaycastHit hit;
+                if (Physics.Raycast(ray, out hit))
+                {
+                    if (!hit.collider.CompareTag("Toothbrush")) continue;
+                    Rigidbody rb = hit.collider.GetComponent<Rigidbody>();
+                    rb.AddForce(Vector3.up * 200);
+                }
+            }
+        }
+    }
+
+    public void HandlePlaneFinderHit(HitTestResult hitTestResult)
+    {
+        if (!objectContainer.activeInHierarchy)
+            _contentPositioningBehaviour.PositionContentAtPlaneAnchor(hitTestResult);
+    }
+
     public void SetActiveObjectContainer(bool value)
     {
         objectContainer.SetActive(value);
