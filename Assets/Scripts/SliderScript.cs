79ab9c8885cf4de35af8cf8b222ab6e950ecce48
ldpunsalan <ldpunsalan@up.edu.ph>
2024-03-02 23:46:03 +0800

    feat(gravity): create non-linear slider with snapping

--- Assets/Scripts/SliderScript.cs
+++ Assets/Scripts/SliderScript.cs
@@ -3,26 +3,61 @@ using System.Collections.Generic;
 using TMPro;
 using UnityEngine;
 using UnityEngine.UI;
+using Unity.Mathematics;
 
 public class SliderScript : MonoBehaviour
 {
     [SerializeField] private Slider _slider;
     [SerializeField] private TextMeshProUGUI _sliderText;
+    private float x = 0;
+    private float y = 9.81f;
+    private float z = 274;
+    private float sliderText;
+
     // Start is called before the first frame update
     void Start()
     {
         _slider.onValueChanged.AddListener((v) =>
         {
-            _sliderText.text = v.ToString("0.00");
+            float A = (x*z - Mathf.Pow(y,2)) / (x - 2*y + z);
+            float B = Mathf.Pow((y-x),2) / (x - 2 * y + z);
+            float C = 2 * Mathf.Log((z-y) / (y-x));
+
+            sliderText = (A + B * Mathf.Exp(C * v));
+            _sliderText.text = sliderText.ToString("0.00") + "m/s²";
         });
     }
 
     // Update is called once per frame
     void Update()
     {
-        if (_slider.value > 9 && _slider.value < 12)
+        float A = (x * z - Mathf.Pow(y, 2)) / (x - 2 * y + z);
+        float B = Mathf.Pow((y - x), 2) / (x - 2 * y + z);
+        float C = 2 * Mathf.Log((z - y) / (y - x));
+
+        // Mars
+        if (sliderText > 1 && sliderText < 5)
         {
-            _slider.value = 9.81f;
+            _slider.value = Mathf.Log((3.7f - A) / B) / C;
         }
+
+        // Earth
+        if (sliderText > 8 && sliderText < 17)
+        {
+            _slider.value = 0.5f;
+        }
+
+        // Jupiter
+        else if (sliderText > 20 && sliderText < 40)
+        {
+            _slider.value = Mathf.Log((24.8f - A) / B) / C;
+        }
+
+        // Sun
+        else if (sliderText > 200 && sliderText < 274)
+        {
+            _slider.value = 1;
+        }
+
     }
 }
