546df11239ed8510569e7305b4049658c3942f2f
CJ Rosales <gargoth13@gmail.com>
2024-04-29 23:22:11 +0800

    feat(FrictionModule): Working level select and level prefabs

--- Assets/Scripts/GameManagerScript.cs
+++ Assets/Scripts/GameManagerScript.cs
@@ -5,26 +5,19 @@ using UnityEngine.SceneManagement;
 
 public class GameManagerScript : Singleton<GameManagerScript>
 {
-    async void Awake()
-    {
-        try
-        {
-            // Initialize the Unity Services SDK
-            await UnityServices.InitializeAsync();
-        }
-        catch (Exception e)
-        {
-            Debug.LogException(e);
-        }
-    }
-    
     public static void DoLog(string log)
     {
         Debug.Log(log);
     }
     
-    public static void MoveToScene(string sceneName) 
+    public static void MoveToScene(string sceneName)
     {
+        Physics.gravity = Vector3.down * 9.81f;
         SceneManager.LoadSceneAsync(sceneName);
+    }
+
+    public void SetSelectedLevel(int num)
+    {
+        PersistentDataContainer.Instance.selectedLevel = num;
     }
 }
