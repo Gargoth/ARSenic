f907bd03fca5a61e52897a093c441a21e744531d
CJ Rosales <gargoth13@gmail.com>
2024-04-29 23:53:58 +0800

    feat(GameManager): Implemented SpawnPopupDialog(string message)
    To edit popup canvas, edit
    `Assets/Resources/Prefabs/Popup.canvas.prefab`
    
    Sample use in Home Scene's Simple Machines button

--- Assets/Scripts/GameManagerScript.cs
+++ Assets/Scripts/GameManagerScript.cs
@@ -1,4 +1,6 @@
 using System;
+using TMPro;
+using Unity.Mathematics;
 using Unity.Services.Core;
 using UnityEngine;
 using UnityEngine.SceneManagement;
@@ -19,5 +21,17 @@ public class GameManagerScript : Singleton<GameManagerScript>
     public void SetSelectedLevel(int num)
     {
         PersistentDataContainer.Instance.selectedLevel = num;
+    }
+
+    public void SpawnPopupDialog(string message)
+    {
+        if (!PersistentDataContainer.Instance.popupCanvasPrefab)
+        {
+            PersistentDataContainer.Instance.popupCanvasPrefab = Resources.Load<GameObject>("Prefabs/Popup Canvas");
+        }
+
+        GameObject newPopup = Instantiate(PersistentDataContainer.Instance.popupCanvasPrefab, Vector3.zero, Quaternion.identity);
+        TextMeshProUGUI textUI = newPopup.transform.Find("Popup Text").GetComponent<TextMeshProUGUI>();
+        textUI.text = message;
     }
 }
