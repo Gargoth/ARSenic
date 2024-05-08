7663f3a54ccc6a00a52f84a76ee1bd9ba75d44ec
ldpunsalan <ldpunsalan@up.edu.ph>
2024-05-08 12:15:16 +0800

    fix(PersistentDataContainer): revise tutorial text

--- Assets/Scripts/PersistentDataContainer.cs
+++ Assets/Scripts/PersistentDataContainer.cs
@@ -19,18 +19,19 @@ public class PersistentDataContainer : Singleton<PersistentDataContainer>
     {
         TopicTutorial["FrictionModule"] = new List<string>
         {
-            "Pick an object to push (sphere or cube).", 
-            "Select a track surface and place it on the road.", 
-            "Hold the fist button to push the object. The longer you hold, the stronger the push!",
-            "Use your knowledge of friction to reach the flag!"
+			"1. Point your camera towards the ground plane. Tap it when you see the square",
+            "2. Tap the sphere/cube icon to change the object.", 
+            "3. Pick a part of the road and choose between the ice, asphalt, and grass to change the selected road.", 
+            "4. Hold the fist button to set an intial push on the object. The longer you hold, the stronger you push!",
+			"5. Click the reset button to try again.",
         }; // List of strings to cycle through
         TopicTutorial["GravityModule"] = new List<string>
         {
-            "Drag the slider to set your chosen gravity.",
-            "Tap the air resistance button to toggle it.",
-            "Tap an object from the inventory to spawn it.",
-            "Tap on a spawned object to make it jump.",
-            "Explore and observe the effects of gravity on different objects!",
+			"1. Point your camera towards the ground plane. Tap it when you see the square",
+            "2. Drag the slider to set the gravity.",
+            "3. Tap the air resistance button to toggle it on/off.",
+            "4. Click the inventory button and select an object you want to spawn.",
+            "5. Tap on a spawned object to make it jump!",
         }; // List of strings to cycle through
     }
 }
